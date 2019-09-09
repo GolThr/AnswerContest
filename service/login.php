56d5a96598643ec3fc1e6a440400a1cff5aa581f
GolThr <1344006993@qq.com>
2019-09-09 17:11:51 +0800

    GolThr(9009)

--- service/login.php
+++ service/login.php
@@ -1,9 +1,39 @@
 <?php
+include("dbConfig.php");
 
-include("dbconfig.php");
-$arr = json_decode($_GET['data'],true);
+class User{
+    public $ifsuccess = '0';
+    public $exam_id;
+    public $name;
+    public $school;
+    public $region;
+    public $score = 0;
+}
 
-var_dump($arr);
+//read_post_data
+$exam_id = $_POST['exam_id'];
+$name = $_POST['name'];
 
-$name = $arr["name"];
+//mysql
+$sql="SELECT * FROM student WHERE exam_id='$exam_id' AND name='$name'";
+$result = $link -> query($sql);
+
+$user = new User();
+if ($result -> num_rows > 0) {
+    $user -> ifsuccess = '1';
+    // 输出每行数据
+    while($row = $result -> fetch_assoc()) {
+        $user -> exam_id = $row['exam_id'];
+        $user -> name = $row['name'];
+        $user -> school = $row['school'];
+        $user -> region = $row['region'];
+    }
+} else {
+    $user -> ifsuccess = '0';
+}
+$link -> close();
+
+//response
+echo json_encode($user);
+
 
