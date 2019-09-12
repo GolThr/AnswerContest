be8904d62ed879def14aecc9da1741b8880fa815
GolThr <1344006993@qq.com>
2019-09-12 17:22:15 +0800

    GolThr(9012)

--- service/getSglSlct.php
+++ service/getSglSlct.php
@@ -22,9 +22,9 @@ echo $paper_id;
 //mysql
 $sql="SELECT * FROM single_select WHERE s_num='$paper_id'";
 $result = $link -> query($sql);
-
-$singleSelect = new SingleSelect();
+$singleSelectArray = Array();
 if ($result -> num_rows > 0) {
+    $singleSelect = new SingleSelect();
     // 输出每行数据
     while($row = $result -> fetch_assoc()) {
         $singleSelect -> id = $row['id'];
@@ -36,9 +36,12 @@ if ($result -> num_rows > 0) {
         $singleSelect -> E = $row['E'];
         $singleSelect -> F = $row['F'];
     }
+    array_push($singleSelectArray, $singleSelect);
 }
 $link -> close();
 
+var_dump($singleSelectArray);
+
 //response
-echo json_encode($singleSelect);
+echo json_encode($singleSelectArray);
 
