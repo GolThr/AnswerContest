e20ed902f4b96f1715aadea0eec0f2aa6d87c4b2
GolThr <1344006993@qq.com>
2019-09-22 22:24:34 +0800

    GolThr(9022)

--- end.php
+++ end.php
@@ -1,12 +1,12 @@
 <?php
 include("dbConfig.php");
 //header('Content-Type: application/json; charset=utf-8');
-$exam_id = $_POST['exam_id'];
+$exam_id = $_GET['exam_id'];
 $sql="SELECT score FROM student where exam_id = '$exam_id'";
 $result=$link->query($sql);
 
 $score=mysqli_fetch_array($result,MYSQLI_ASSOC);
-echo $score;
+var_dump( $score);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,7 +16,7 @@ echo $score;
 </head>
 <body>
 <h1>考试已结束</h1>
-<h2>你目前客观题分数为：<?php echo $score; ?></h2>
+<h2>你目前客观题分数为：<?php echo $score['score']; ?></h2>
 <h2>具体分数还请成绩公布之后查询！</h2>
 </body>
 </html>
